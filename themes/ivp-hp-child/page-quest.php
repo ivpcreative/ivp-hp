821df96020d9b874e1478d5d473f58f421b2032e
IVP-WAKAI-PC\wakai <ivpcreative@gmail.com>
2017-08-24 13:22:27 +0900

    wakai:php

--- themes/ivp-hp-child/page-quest.php
+++ themes/ivp-hp-child/page-quest.php
@@ -1,16 +1,21 @@
 <?php
 /**
  * The template for displaying pages
-  *Template Name:page-ec
+  *Template Name:page-quest
  *
  * @package WordPress
- * @subpackage ivp-hp
+ * @subpackage ivp-hp-child
  * @since ivp-hp 1.0
  */
 
 get_header(); ?>
 
-
+<?php
+// Start the loop.
+while ( have_posts() ) : the_post();
+the_content();
+endwhile;
+?>
 
 <?php // get_sidebar(); ?>
 <?php get_footer(); ?>
