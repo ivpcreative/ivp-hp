55b3e1d742db9817864b9622077939f17cf55c2f
IVP-WAKAI-PC\wakai <ivpcreative@gmail.com>
2017-08-24 14:33:35 +0900

    wakai:php

--- themes/ivp-hp-child/index.php
+++ themes/ivp-hp-child/index.php
@@ -9,7 +9,47 @@
  */
 
 get_header(); ?>
+<ul id="slider">
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide1.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide1_txt.png" width="auto" height="300" alt="海を飛び越えて、挑戦・貢献する。"/></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide2.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide2_txt.png" width="auto" height="300" alt="ECシステム開発"/><div><a href="ec/">MORE</a></div></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide3.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide3_txt.png" width="auto" height="300" alt="IoTサービス開発"/><div><a href="iot/">MORE</a></div></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide4.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide4_txt.png" width="auto" height="300" alt="WEBシステム開発"/><div><a href="web/">MORE</a></div></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide5.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide5_txt.png" width="auto" height="300" alt="24×365運用保守"/><div><a href="maint/">MORE</a></div></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide6.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide6_txt.png" width="auto" height="300" alt="セキュリティ運用保守"/></li>
+  <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide7.jpg) center bottom no-repeat; background-size:cover;"><img src="<?php echo get_stylesheet_directory_uri()?>/images/slide7_txt.png" width="auto" height="300" alt="オフショア開発"/><div><a href="offshore/">MORE</a></div></li>
+</ul>
 
+<div class="slide_left">
+  <h1><a href=""><img src="<?php echo get_stylesheet_directory_uri()?>/images/logo.png" width="181" height="43" alt="IVP Internet Value Provider　IVP Co. Ltd." title="IVP Internet Value Provider　IVP Co. Ltd." /></a></h1>
+
+  <ul class="sf-menu">
+    <li class="first"><a href="/">HOME</a><img src="<?php echo get_stylesheet_directory_uri()?>/images/menu_line.png" width="112" height="1" alt="menu_line"/></li>
+    <h4><li><a href="/company/">会社情報</a></li></h4>
+    <h4><li><a href="/access/">アクセスマップ</a></li></h4>
+    <li><h4><span>システム開発/スクラッチ開発</span></h4>
+      <ul>
+        <h4><li><a href="/ec/">ECシステム開発</a></li></h4>
+        <h4><li><a href="/iot/">loTサービス開発</a></li></h4>
+        <h4><li><a href="/web/">WEBシステム開発</a></li></h4>
+      </ul>
+    </li>
+    <li><h4><span>セキュリティ/運用保守</span></h4>
+      <ul>
+        <h4><li><a href="/maint/">24×365運用保守</a></li></h4>
+        <h4><li><a href="/offshore/">オフショア開発</a></li></h4>
+      </ul>
+    </li>
+    <li><h4><a href="https://ww2.ivp.co.jp/recruit/" target="_blank">採用情報</a></h4></li>
+    <li class="last"><h4><a href="/partner/">協力会社募集</a></h4></li>
+  </ul>
+
+  <div class="sf-bottom">
+      <p id="fixed">代表電話(東京本社)<strong>03-5469-8601</strong><span>営業時間:10:00-19:00（平日)</span></p>
+      <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'leftnavi', 'click', 'quest', 1, {'nonInteraction': 1});"><i class="fa fa-envelope" aria-hidden="true"></i>お問い合せ</a>
+      <a href="https://ww2.ivp.co.jp/english/" target="_blank"><i class="fa fa-globe" aria-hidden="true"></i>English</a>
+  </div>
+</div>
+<div class="slide_left_after"></div>
 <div class="scroll_icon">
         <img src="<?php echo get_stylesheet_directory_uri()?>/images/scroll_icon.png" width="116" height="60" alt="scroll icon"/>
     </div>
