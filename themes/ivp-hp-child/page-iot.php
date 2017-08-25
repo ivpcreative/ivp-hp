55819a178a7044496d72738a9b66df388e7af6c5
alyhugo <hugoalyannamarie@yahoo.com>
2017-08-25 18:33:15 +0900

    iot ページの画像

--- themes/ivp-hp-child/page-iot.php
+++ themes/ivp-hp-child/page-iot.php
@@ -68,76 +68,76 @@ get_header(); ?>
 <!-- モーダル専用エリア -->
 <div id="modal-options1" data-izimodal-group="group1" data-izimodal-loop="" data-izimodal-title="ＬＩＮＥビーコンを活用したＩｏＴサービス">
 	<div class="modal-base">
-		<img src="../images/iot_img01.png" width="100%" height="auto" alt="lineビーコン" class="iot02_img mt-40"/>
+		<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_img01.png" width="100%" height="auto" alt="lineビーコン" class="iot02_img mt-40"/>
         <p>「LINE Beacon」とは、LINEビジネスコネクトの機能の一種で、店舗などに設置されたbeaconからデータを受信し、商品やクーポン情報を顧客に届けることが出来る。<br />さらに位置情報を使った新しい顧客とのコミュニケーションを取ることが出来るため、顧客に適切なタイミングで情報を提供したり、位置情報を利用した新しいサービスが利用できる。今後、さまざまなマーケティングシーンでの展開が期待されている。 （beaconとは、周辺機器をワイヤレスで使える近距離通信規格のBluetooth®の発信機。顧客がbeaconに近づくと、スマートフォンに商品やクーポンの情報が届く。なお、スマートフォンのBluetoothをオンにしていなければ、受信はできない。）</p>
     	<h3 align="center" class="modalh3 cblue">システム連携イメージ</h3>
-		<img src="../images/iot_modal_img00.png" width="100%" height="auto" alt="システム連携イメージ" class="modalImg80"/>
+		<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img00.png" width="100%" height="auto" alt="システム連携イメージ" class="modalImg80"/>
         <h3 align="center" class="modalh3 cblue">活用イメージ</h3>
       	<div class="imgs clearfix">
         	<div>
-            	<img src="../images/iot_modal_img01.jpg" width="100%" height="auto" alt="商品詳細情報を配布"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img01.jpg" width="100%" height="auto" alt="商品詳細情報を配布"/>
             	<h4>商品詳細情報を配布</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img02.jpg" width="100%" height="auto" alt="絵に近づくと詳細情報を配布"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img02.jpg" width="100%" height="auto" alt="絵に近づくと詳細情報を配布"/>
             	<h4>絵に近づくと詳細情報を配布</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img03.jpg" width="100%" height="auto" alt="店舗にてポイントを発行"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img03.jpg" width="100%" height="auto" alt="店舗にてポイントを発行"/>
             	<h4>店舗にてポイントを発行</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img04.jpg" width="100%" height="auto" alt="店頭にて限定クーポンを配布"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img04.jpg" width="100%" height="auto" alt="店頭にて限定クーポンを配布"/>
             	<h4>店頭にて限定クーポンを配布</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img05.jpg" width="100%" height="auto" alt="ユーザーの位置情報を記録"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img05.jpg" width="100%" height="auto" alt="ユーザーの位置情報を記録"/>
             	<h4>ユーザーの位置情報を記録</h4>
             </div>
         </div>
-        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="../images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
+        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
       
   </div
 ></div>
 <div id="modal-options2" data-izimodal-group="group1" data-izimodal-loop="" data-izimodal-title="商業施設・イベント向けＩｏＴサービス ">
 	<div class="modal-base">
-    	<img src="../images/iot_modal_photo01.jpg" width="100%" height="auto" alt="商業施設・イベント向け" class="mb20"/>
+    	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_photo01.jpg" width="100%" height="auto" alt="商業施設・イベント向け" class="mb20"/>
         <p>「商業施設・イベント向けＩoＴサービス」は、ビーコンやセンサー、ゲートウェイ機器、クラウドシステムなどを組み合わせて、人やモノの位置、加速度など様々な情報を取得・分析をした結果、「領域に入った」もしくは「領域外になった」といった情報通知で、企業・学校・施設・イベントなどの入退場管理を行うことが可能。 また、顧客に適切なタイミングで商品やクーポン情報を提供したり、位置情報を利用した新しいサービスが利用できる。</p>
     	<h3 align="center" class="modalh3 cblue">機能例</h3>
       	<div class="imgs clearfix">
         	<div>
-            	<img src="../images/iot_modal_img06.jpg" width="100%" height="auto" alt="クーポン・ポイントの発行"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img06.jpg" width="100%" height="auto" alt="クーポン・ポイントの発行"/>
             	<h4>クーポン・ポイントの発行</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img07.jpg" width="100%" height="auto" alt="顧客の導線分析"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img07.jpg" width="100%" height="auto" alt="顧客の導線分析"/>
             	<h4>クーポン・ポイントの発行</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img08.jpg" width="100%" height="auto" alt="道案内・混雑状況の可視化"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img08.jpg" width="100%" height="auto" alt="道案内・混雑状況の可視化"/>
             	<h4>道案内・混雑状況の可視化</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img09.jpg" width="100%" height="auto" alt="音声ガイド・観光ガイド"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img09.jpg" width="100%" height="auto" alt="音声ガイド・観光ガイド"/>
             	<h4>音声ガイド・観光ガイド</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img10.jpg" width="100%" height="auto" alt="ゲーム・アトラクション"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img10.jpg" width="100%" height="auto" alt="ゲーム・アトラクション"/>
             	<h4>ゲーム・アトラクション</h4>
             </div>
         	<div>
-            	<img src="../images/iot_modal_img11.jpg" width="100%" height="auto" alt="広告・デジタルサイネージ"/>
+            	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img11.jpg" width="100%" height="auto" alt="広告・デジタルサイネージ"/>
             	<h4>広告・デジタルサイネージ</h4>
             </div>
         </div>
-        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="../images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
+        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
       
   </div>
 </div>
 
 <div id="modal-options3" data-izimodal-group="group1" data-izimodal-loop="" data-izimodal-title="アジャイル開発×ＩｏＴサービス">
 	<div class="modal-base">
-    	<img src="../images/iot_modal_photo02.png" width="100%" height="auto" alt="アジャイル開発×ＩＯＴサービス" class="mb20"/>
+    	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_photo02.png" width="100%" height="auto" alt="アジャイル開発×ＩＯＴサービス" class="mb20"/>
         <ul>
         <li>お客さまの運用に合わせたカスタマイズが可能</li>
         <li>基幹システム、物流システム、コールセンター、在庫一元管理システム、レコメンドシステムなどの外部システムとの連携が可能</li>
@@ -147,7 +147,7 @@ get_header(); ?>
         </ul>
         <h3 align="center" class="modalh3 cblue">ＩｏＴプロト開発とは？</h3>
         <div class="clearfix agile">
-        	<img src="../images/iot_modal_img12.png" width="100%" height="auto" alt="IoTプロト開発"/>
+        	<img src="<?php echo get_stylesheet_directory_uri()?>/images/iot_modal_img12.png" width="100%" height="auto" alt="IoTプロト開発"/>
             <p>アイブイピーの「アジャイル×IoTサービス」は、お客さまのIoTサービスの「初めの一歩」を支援するために、 初めから完成度の高いシステムをあえて作り込まず、「オフショア開発で試しながら」作り上げていく開発サービス。</p>
         </div>
         <h3 align="center" class="modalh3 cblue">アジャイル開発のメリット</h3>
@@ -158,7 +158,7 @@ get_header(); ?>
                 <td>スピーディー</td>
             </tr>
         </table>
-        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="../images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
+        <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'iot', 'click', 'quest', 1, {'nonInteraction': 1});" class="links"><span>お問い合せフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></a>
   </div>
 </div>
 
