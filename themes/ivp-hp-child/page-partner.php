840aa14facf3be1024aa532e5d3dabf42fe6ed90
alyhugo <hugoalyannamarie@yahoo.com>
2017-08-30 11:58:42 +0900

    ページ partner の画像

--- themes/ivp-hp-child/page-partner.php
+++ themes/ivp-hp-child/page-partner.php
@@ -12,7 +12,7 @@ get_header(); ?>
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri()?>/css/second.css"/>
 
 <ul id="slider2">
-    <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide7.jpg) center bottom no-repeat; background-size:cover;"><h2><img src="<?php echo get_stylesheet_directory_uri()?>/jsphp echo get_stylesheet_directory_uri()?>/images/second_title_partner.png" width="auto" height="200" alt="ビジネスパートナー募集"/></h2></li>
+    <li style="background:url(<?php echo get_stylesheet_directory_uri()?>/images/slide7.jpg) center bottom no-repeat; background-size:cover;"><h2><img src="<?php echo get_stylesheet_directory_uri()?>/images/second_title_partner.png" width="auto" height="200" alt="ビジネスパートナー募集"/></h2></li>
 </ul>
 <?php get_template_part( 'template-parts/content', 'slider_left' ); ?>
 <div class="slide_left_after2"></div>
@@ -25,13 +25,13 @@ get_header(); ?>
             <p>アイブイピーではお客さまの多様なニーズにお応えするために、<br />
             IoTサービス開発や、WEBシステム開発にご協力頂ける<br />
             ビジネスパートナー企業さまを幅広く募集しています。</p>
-            <strong class="clink"><span>お問い合わせフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/jsphp echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></strong>
+            <strong class="clink"><span>お問い合わせフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></strong>
         </a>
         <div id="partner1"></div>
         <a href="https://reg31.smp.ne.jp/regist/is?SMPFORM=qck-silcp-d23ee8f7d2f6fb629a9cd2f0ca70d828" target="_blank" onclick="ga('send', 'event', 'engineer', 'click', 'quest', 1, {'nonInteraction': 1});" class="partner02 clearfix">
             <h3>フリーランスエンジニア募集 </h3>
             <p>フリーランスとして定期的にアイブイピーの開発案件にご協力頂ける<br />プロジェクトマネージャーさま、ディレクターさま、エンジニアさま<br />など幅広く募集しています。</p>
-            <strong class="clink"><span>お問い合わせフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/jsphp echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></strong>
+            <strong class="clink"><span>お問い合わせフォーム</span><img src="<?php echo get_stylesheet_directory_uri()?>/images/arrow_white.png" width="16" height="7" alt="arrow"/></strong>
         </a>
 
         <?php // get_sidebar(); ?>
